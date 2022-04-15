9a6bdb0a753528dace70f3cab34154860b0fc478
CANTIMPLORA <emmanuel.manzanilla.7e5@itb.cat>
2022-04-15 19:24:40 +0200

    js

---
+++ java_script/provalert.js
@@ -0,0 +1,9 @@
+// for (inicialización; condición; incremento)
+for (i = 0; i < 5; i++) {
+    console.log("Valor de i:", i);
+ }
+ 
+ // doble contador
+ for (i = 0, j = 5; i < 5; i++, j--) {
+    console.log("Valor de i y j:", i, j);
+ } 
