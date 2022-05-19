7d94471e90fe92046c4c0ff0ceee017a9436e6f8
CANTIMPLORA <emmanuel.manzanilla.7e5@itb.cat>
2022-05-19 07:27:17 +0200

    practika

--- AdivinaCodigoSecreto-Enunciado/script.js
+++ AdivinaCodigoSecreto-Enunciado/script.js
@@ -1,64 +1,61 @@
-//Umar Mohammad Riaz    1rB ASIXc
+const code = [];
 
-const codigo = [];
+var count = 0
 
-var contador = 0
-
-const intentosFijos = 5 //Refractor de intentos por intentosFijos para que el usuario no los elija.
-var intentos = parseInt(window.prompt("¿Cuántos intentos quieres hacer? "))
-var intentosRealizados = 0
+const fixed = 5 //Refactorizacion de intentos por numero fijo de chances para que el usuario no los pueda elegir.
+var chances = parseInt(window.prompt("¿Cuántos intentos quieres hacer? "))
+var donechances = 0
 
 
-//Comprobamos que el num de intentos es un digito
-if (parseInt(intentos) != intentos){
+//Check integer
+if (parseInt(chances) != chances){
     if(!alert("¡Debes introducir un número!\nEjemplo: 5")){window.location.reload();}
 }
 
 
-/*1. Genera una constante CODIGO_SECRETO de tipo array de 5
-Número aleatorios entre 0 y 9 usando la libreria Math.random();*/
+/*1. Genera una constante CODIGO_SECRETO de tipo array de 5 números aleatorios entre 0 y 9 usando la libreria Math.random();*/
 
-function codigoSecreto() {
+function CODIGO_SECRETO() {
     for (let i = 0; i < 5; i++) {
-        codigo[i] = Math.floor((Math.random() * 10));
+        code[i] = Math.floor((Math.random() * 10));
     }
     console.log(Math.floor([1 ,2,3,4]));
-    console.log(codigo)
+    console.log(code)
 }
 
 
 function Comprobar(){
     var numeroIntroducido = document.getElementById("numero").value;
-    intentosRealizados ++
+    donechances ++
     if (numeroIntroducido.length != 5){
-        alert("¡Debes poner 5 dígitos!")
+        alert("Tienes que introducir 5 dígitos numéricos!")
 
     }
     else{
-        if (intentos > 0){
-            confirmarComprobación(numeroIntroducido)
-            contarNum(numeroIntroducido)
-            document.getElementById("info").textContent = "Intentos restantes: " + Number(intentos - 1)
-            if (AciertosTotales(numeroIntroducido) == "true"){
-                intentos = 0;
-                document.getElementById("info").textContent = "¡HAS GANADO! :)"
+        if (chances > 0){
+            confirmingCheck(numeroIntroducido)
+            countNum(numeroIntroducido)
+            document.getElementById("info").textContent = "Remaining chances...: " + Number(chances - 1)
+            if (TotalOKNumbah(numeroIntroducido) == "true"){
+                chances = 0;
+                document.getElementById("info").textContent = "GANASTE!"
                 document.getElementById("info").parentNode.style.backgroundColor = "green"
             }
         }
         else{
-            document.getElementById("info").textContent = "¡HAS PERDIDO! :("
-            numerosFallidos(codigo)
+            document.getElementById("info").textContent = "Oh you suck (perdiste)"
+            failedNumbah(code)
         }
-        intentos = intentos -1
+        chances = chances -1
     }
 }
 
 
-function contarNum(numeros){
+function countNum(numeros){
     for (numero of numeros){
         var cantidadDeNumeros = 0
         for (i=0;i<5;i++){
-            if(numero == codigo[i]){
+            if(numero == code[i]){
                 cantidadDeNumeros = cantidadDeNumeros + 1
             }
         }
@@ -69,10 +66,10 @@ function contarNum(numeros){
 }
 
 
-function confirmarComprobación(numeros){
+function confirmingCheck(numeros){
     for (i=0;i<5;i++){
-        if (numeros[i] == codigo[i]){
-            document.getElementById("codigo").children[0].children[i].children[0].textContent = codigo[i]
+        if (numeros[i] == code[i]){
+            document.getElementById("codigo").children[0].children[i].children[0].textContent = code[i]
         }
     }
     numerosAcertados(numeros)
@@ -80,25 +77,25 @@ function confirmarComprobación(numeros){
 }
 
 
-function numerosAcertados(numero){
+function numerosAcertados(numero){ //parte vacía del html donde va metiendo los divs
     document.getElementById("Result").innerHTML += "<div class='rowResult w100 flex wrap'>\n<div class='w20'>\n<div class='celResult flex'></div>\n</div>\n<div class='w20'>\n<div class='celResult flex'></div>\n</div>\n<div class='w20'>\n<div class='celResult flex'></div>\n</div>\n<div class='w20'>\n<div class='celResult flex'></div>\n</div>\n<div class='w20'>\n<div class='celResult flex'></div>\n</div>\n</div>"
     for (i=0;i<5;i++){
-        document.getElementById("Result").children[contador].children[i].children[0].textContent = numero[i]
-        if (numero[i] == codigo[i]){
-            document.getElementById("Result").children[contador].children[i].children[0].style.backgroundColor ="green"
+        document.getElementById("Result").children[count].children[i].children[0].textContent = numero[i]
+        if (numero[i] == code[i]){
+            document.getElementById("Result").children[count].children[i].children[0].style.backgroundColor ="green"
         }
-        else if (codigo.includes(parseInt(numero[i]))== true){
-            document.getElementById("Result").children[contador].children[i].children[0].style.backgroundColor ="yellow"
+        else if (code.includes(parseInt(numero[i]))== true){
+            document.getElementById("Result").children[count].children[i].children[0].style.backgroundColor ="yellow"
         }
         else{
-            document.getElementById("Result").children[contador].children[i].children[0].style.backgroundColor ="red"
+            document.getElementById("Result").children[count].children[i].children[0].style.backgroundColor ="red"
         }
     }
-    contador += 1
+    count += 1
 }
 
 
-function numerosFallidos(numero){
+function failedNumbah(numero){
     document.getElementById("codigo").innerHTML = "<div class='w100 flex wrap'>\n<div class='w20'>\n<div class='cel flex'>"+ numero[0] +"</div>\n</div>\n<div class='w20'>\n<div class='cel flex'>"+ numero[1] +"</div>\n</div>\n<div class='w20'>\n<div class='cel flex'>"+ numero[2] +"</div>\n</div>\n<div class='w20'>\n<div class='cel flex'>"+ numero[3] +"</div>\n</div>\n<div class='w20'>\n<div class='cel flex'>"+ numero[4] +"</div>\n</div>\n</div>"
     for (i=0;i<5;i++){
         document.getElementById("codigo").children[0].children[i].children[0].style.backgroundColor = "red"
@@ -106,10 +103,10 @@ function numerosFallidos(numero){
 }
 
 
-function AciertosTotales(numeros){
+function TotalOKNumbah(numeros){
     let aciertos = 0
     for (i=0;i<5;i++){
-        if (numeros[i] == codigo[i]){
+        if (numeros[i] == code[i]){
             aciertos += 1
         }
     }
@@ -119,20 +116,20 @@ function AciertosTotales(numeros){
 }
 
 
-function adivinaLaPosicion(){
+function guessLocation(){
     var posicion = window.prompt("Posición (0-4) ")
     var numero = window.prompt("Número de la posición " + posicion + " (0-9)")
     numero = parseInt(numero)
-    if (numero == codigo[posicion]){
-        alert("¡Has adivinado!")
+    if (numero == code[posicion]){
+        alert("Lo adivinaste!")
     }
-    else if(codigo.includes(numero) == false){
-        alert("¡No está ese número!")
+    else if(code.includes(numero) == false){
+        alert("Ese número NO!")
     }
     else{
-        alert("¡Existe, peró en otra posición!")
+        alert("Número correcto pero posición incorrecta :/")
     }
 }
 
 
-codigoSecreto();
+CODIGO_SECRETO();
